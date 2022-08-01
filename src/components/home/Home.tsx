30e131721a6650c61248e23c354d985a5b2d30c7
Oshin Gansi <os.gansi@gmail.com>
2022-08-02 00:09:53 +0545

    adds create new patient form and patient list

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,25 +1,25 @@
-import { Button, Form } from "antd";
+import { Button } from "antd";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
-  console.log(isLoggedIn);
-  const onFinish = (values: any) => {
-    // console.log(values);
-    navigate("/create");
-  };
-
   return (
     <div>
-      {/* <h1>Hello</h1> */}
-      <Form onFinish={onFinish}>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Create New PatientID
-          </Button>
-        </Form.Item>
-      </Form>
+      <Button
+        type="primary"
+        onClick={() => navigate("/create")}
+        htmlType="submit"
+      >
+        Create New PatientID
+      </Button>
+
+      <Button
+        type="primary"
+        onClick={() => navigate("/patientlist")}
+        htmlType="submit"
+      >
+        Patient List
+      </Button>
     </div>
   );
 }
